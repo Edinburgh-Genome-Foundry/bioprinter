ebb34fb2d79a9f9998cf302cd4ba368f01102883
Peter Vegh <pvegh@posteo.uk>
2020-09-11 10:58:06 +0100

    License autodetection fix

--- setup.py
+++ setup.py
@@ -1,17 +1,20 @@
 import ez_setup
+
 ez_setup.use_setuptools()
 
 from setuptools import setup, find_packages
 
-exec(open('bioprinter/version.py').read())  # loads __version__
+exec(open("bioprinter/version.py").read())  # loads __version__
 
-setup(name='bioprinter',
-      version=__version__,
-      author='Zulko',
-      description='',
-      long_description=open('README.rst').read(),
-      license='MIT',
-      url='https://github.com/Edinburgh-Genome-Foundry/bioprinter',
-      keywords="bioprinter pointillism living art",
-      packages=find_packages(exclude='docs'),
-      install_requires=['numpy', 'Pillow', 'pytest'])
+setup(
+    name="bioprinter",
+    version=__version__,
+    author="Zulko",
+    description="",
+    long_description=open("README.rst").read(),
+    license="MIT",
+    url="https://github.com/Edinburgh-Genome-Foundry/bioprinter",
+    keywords="bioprinter pointillism living art",
+    packages=find_packages(exclude="docs"),
+    install_requires=["numpy", "Pillow", "pytest"],
+)
